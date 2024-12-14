ad2c97dd4bf6c9882690a6b866fe2d6fbe03d38e
Mathan31 <mathan.chandrasekar@gmail.com>
2024-12-14 09:11:41 +0530

    Made changes to the FirstProgram

--- GitJavaProject/src/day01/FirstProgram.java
+++ GitJavaProject/src/day01/FirstProgram.java
@@ -7,7 +7,7 @@ public class FirstProgram {
 		System.out.println("Locator changes done by mathan");
 		System.out.println("This is My First Program and quick fix done by deepika");
 		
-
+		System.out.println("New branch created in Remote");
 	}
 
 }
