32117b5566b494c3d72cbd9653731931dc1d0bce
Mathan31 <mathan.chandrasekar@gmail.com>
2024-12-14 09:05:28 +0530

    Locator fix

--- GitJavaProject/src/day01/FirstProgram.java
+++ GitJavaProject/src/day01/FirstProgram.java
@@ -4,7 +4,9 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		System.out.println("This is My First Program");
-		
+
+		System.out.println("Locator changes done by mathan");
+
 	}
 
 }
